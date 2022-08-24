263410839e87a90541bc17b8b13fd20465c6e6dd
Keelen Fisher <keelencarrera@gmail.com>
2022-08-24 12:44:57 -0500

    added in comments for better understanding

--- server.js
+++ server.js
@@ -3,14 +3,17 @@
 console.log('testing testing, is this thing on? or not?');
 
 // REQUIRES - 
+//  We use require instead of import 
 const express = require('express');
 require('dotenv').config();
 let data = require('./data/weather.json');
 const cors = require('cors');
 
+// Invoke the library,  so app will become our server.
 const app = express();
 
-// middleware to share resources across the internet 
+// middleware to share resources across the internet: The most important job for middleware is to stop the request incase theres an error in the require.  
+
 app.use(cors());
 
 let PORT = process.env.PORT || 3005;
@@ -20,6 +23,8 @@ let PORT = process.env.PORT || 3005;
 
 // Base route
 // Proof of Life
+
+//.get: One of the REST API, the rest contains watys to request informaton in the query. 
 app.get('/', (request, response) => {
   console.log('This works!');
   response.status(200).send('Welcome to our server');
@@ -35,6 +40,8 @@ app.get('/', (request, response) => {
 app.get('/weather', (request, response) => {
   try 
   {
+    //The "city" needs to be in the URL. 
+    // The "request.query.city" NEEDS to be there and 
     let nameOCity = request.query.city;
     // console.log('weather.json: ', data[0])
     // console.log('parameters', request.params, request.query);
